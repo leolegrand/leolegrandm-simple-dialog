e1cb94d1112ea11ae61b25ddce85eaac8ce7dbd1
leolegrand <88097360+leolegrand@users.noreply.github.com>
2022-08-21 15:10:00 +0200

    build version adjustments to allow export of jsdocs

--- dist/components/Dialog.js
+++ dist/components/Dialog.js
@@ -1,27 +1,69 @@
-"use strict";
+'use strict'
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
+Object.defineProperty(exports, '__esModule', {
+  value: true,
+})
+exports.default = void 0
 
-require("core-js/modules/web.dom-collections.iterator.js");
+require('core-js/modules/web.dom-collections.iterator.js')
 
-var _react = _interopRequireWildcard(require("react"));
+var _react = _interopRequireWildcard(require('react'))
 
-var _cookies = _interopRequireDefault(require("./cookies.png"));
+var _cookies = _interopRequireDefault(require('./cookies.png'))
 
-var _succes = _interopRequireDefault(require("./succes.png"));
+var _succes = _interopRequireDefault(require('./succes.png'))
 
-var _errorMessage = _interopRequireDefault(require("./error-message.png"));
+var _errorMessage = _interopRequireDefault(require('./error-message.png'))
 
-require("./modal.css");
+require('./modal.css')
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj }
+}
 
-function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+function _getRequireWildcardCache(nodeInterop) {
+  if (typeof WeakMap !== 'function') return null
+  var cacheBabelInterop = new WeakMap()
+  var cacheNodeInterop = new WeakMap()
+  return (_getRequireWildcardCache = function _getRequireWildcardCache(
+    nodeInterop
+  ) {
+    return nodeInterop ? cacheNodeInterop : cacheBabelInterop
+  })(nodeInterop)
+}
 
-function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+function _interopRequireWildcard(obj, nodeInterop) {
+  if (!nodeInterop && obj && obj.__esModule) {
+    return obj
+  }
+  if (obj === null || (typeof obj !== 'object' && typeof obj !== 'function')) {
+    return { default: obj }
+  }
+  var cache = _getRequireWildcardCache(nodeInterop)
+  if (cache && cache.has(obj)) {
+    return cache.get(obj)
+  }
+  var newObj = {}
+  var hasPropertyDescriptor =
+    Object.defineProperty && Object.getOwnPropertyDescriptor
+  for (var key in obj) {
+    if (key !== 'default' && Object.prototype.hasOwnProperty.call(obj, key)) {
+      var desc = hasPropertyDescriptor
+        ? Object.getOwnPropertyDescriptor(obj, key)
+        : null
+      if (desc && (desc.get || desc.set)) {
+        Object.defineProperty(newObj, key, desc)
+      } else {
+        newObj[key] = obj[key]
+      }
+    }
+  }
+  newObj.default = obj
+  if (cache) {
+    cache.set(obj, newObj)
+  }
+  return newObj
+}
 
 // REACT
 // MEDIA
@@ -38,93 +80,123 @@ To use pre-created modals, use strings with the words "errors", "'cookies" or "s
  * @param callToActionCallback - The function that is called when you press the call-to-action button & cross icon.
  * @returns { React.ReactElement } A dialog component
  */
-const Dialog = _ref => {
+const Dialog = (_ref) => {
   let {
     modal,
     closeIcon,
     callToAction,
     allowCustomization,
-    callToActionCallback
-  } = _ref;
-  const [shown, setShown] = (0, _react.useState)(true); // Defaults modal that the user can pick from
+    callToActionCallback,
+  } = _ref
+  const [shown, setShown] = (0, _react.useState)(true) // Defaults modal that the user can pick from
 
-  const modalOptions = [{
-    title: 'cookies',
-    icon: _cookies.default,
-    altText: 'cookies image',
-    content: 'We use cookies for improving user experience, analytics and marketing.',
-    cta: 'Yummy !'
-  }, {
-    title: 'succes',
-    icon: _succes.default,
-    altText: 'check image',
-    content: 'Your request has been successfully completed.',
-    cta: 'Got it !'
-  }, {
-    title: 'error',
-    icon: _errorMessage.default,
-    altText: 'error image',
-    content: 'Oops ! Something went wrong.',
-    cta: 'Try again !'
-  }]; // toggle if the dialog is visible or not, a callback function can be added on the close/cta button
+  const modalOptions = [
+    {
+      title: 'cookies',
+      icon: _cookies.default,
+      altText: 'cookies image',
+      content:
+        'We use cookies for improving user experience, analytics and marketing.',
+      cta: 'Yummy !',
+    },
+    {
+      title: 'succes',
+      icon: _succes.default,
+      altText: 'check image',
+      content: 'Your request has been successfully completed.',
+      cta: 'Got it !',
+    },
+    {
+      title: 'error',
+      icon: _errorMessage.default,
+      altText: 'error image',
+      content: 'Oops ! Something went wrong.',
+      cta: 'Try again !',
+    },
+  ] // toggle if the dialog is visible or not, a callback function can be added on the close/cta button
 
   const handleShown = () => {
-    setShown(false);
+    setShown(false)
 
     if (callToActionCallback) {
-      callToActionCallback();
+      callToActionCallback()
     }
-  }; // ON ESCAPE KEYPRESS, THE MODAL IS CLOSED
+  } // ON ESCAPE KEYPRESS, THE MODAL IS CLOSED
 
-
-  (0, _react.useEffect)(() => {
-    const handleKeyPress = event => {
+  ;(0, _react.useEffect)(() => {
+    const handleKeyPress = (event) => {
       if (event.keyCode === 27) {
-        handleShown();
+        handleShown()
       }
-    };
+    }
 
-    window.addEventListener('keydown', handleKeyPress);
+    window.addEventListener('keydown', handleKeyPress)
     return () => {
-      window.removeEventListener('keydown', handleKeyPress);
-    };
-  });
-  let option;
+      window.removeEventListener('keydown', handleKeyPress)
+    }
+  })
+  let option
 
   if (allowCustomization) {
-    option = modal;
+    option = modal
   } else {
-    let selectedModalOption = modalOptions.filter(i => i.title === modal);
-    option = selectedModalOption[0];
+    let selectedModalOption = modalOptions.filter((i) => i.title === modal)
+    option = selectedModalOption[0]
   }
 
   if (shown) {
-    return /*#__PURE__*/_react.default.createElement("dialog", {
-      open: true,
-      className: "modal__bg",
-      id: "myDialog"
-    }, /*#__PURE__*/_react.default.createElement("div", {
-      className: "modal",
-      id: "modal"
-    }, closeIcon ? /*#__PURE__*/_react.default.createElement("button", {
-      onClick: () => handleShown(),
-      className: "modal__close-btn"
-    }, "\u2716") : null, /*#__PURE__*/_react.default.createElement("img", {
-      className: "modal__img",
-      src: option.icon,
-      alt: option.altText
-    }), /*#__PURE__*/_react.default.createElement("p", {
-      className: "modal__body"
-    }, option.content), callToAction ? /*#__PURE__*/_react.default.createElement("button", {
-      onClick: () => handleShown(),
-      className: 'modal__cta modal__cta--' + option.title
-    }, option.cta) : null));
+    return /*#__PURE__*/ _react.default.createElement(
+      'dialog',
+      {
+        open: true,
+        className: 'modal__bg',
+        id: 'myDialog',
+      },
+      /*#__PURE__*/ _react.default.createElement(
+        'div',
+        {
+          className: 'modal',
+          id: 'modal',
+        },
+        closeIcon
+          ? /*#__PURE__*/ _react.default.createElement(
+              'button',
+              {
+                onClick: () => handleShown(),
+                className: 'modal__close-btn',
+              },
+              '\u2716'
+            )
+          : null,
+        /*#__PURE__*/ _react.default.createElement('img', {
+          className: 'modal__img',
+          src: option.icon,
+          alt: option.altText,
+        }),
+        /*#__PURE__*/ _react.default.createElement(
+          'p',
+          {
+            className: 'modal__body',
+          },
+          option.content
+        ),
+        callToAction
+          ? /*#__PURE__*/ _react.default.createElement(
+              'button',
+              {
+                onClick: () => handleShown(),
+                className: 'modal__cta modal__cta--' + option.title,
+              },
+              option.cta
+            )
+          : null
+      )
+    )
   } else {
-    return /*#__PURE__*/_react.default.createElement("dialog", {
-      open: false
-    });
+    return /*#__PURE__*/ _react.default.createElement('dialog', {
+      open: false,
+    })
   }
-};
+}
 
-var _default = Dialog;
-exports.default = _default;
+exports.default = Dialog
